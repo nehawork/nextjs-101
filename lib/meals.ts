2739c3c29a7847a52c63dcca12a5cac144dfbe86
nehawork <neharamchandani.work@gmail.com>
2026-08-19 21:52:09 +0530

    Added | Meal Details Page | With Data and Not Found Handlers

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,11 +1,18 @@
-import { MealType } from '@/app/types/meals.types';
+import { MealDetailsType, MealType } from '@/app/types/meals.types';
 import sql from 'better-sqlite3';
 
 const db = sql('meals.db');
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const stmt = db.prepare('SELECT * FROM meals');
   return stmt.all() as MealType[];
   // throw new Error('Simulated error for testing purposes'); // Simulate an error
+}
+
+export async function getMeal(slug: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return db
+    .prepare('SELECT * FROM meals WHERE slug = ?')
+    .get(slug) as MealDetailsType;
 }
--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -1,9 +1,43 @@
-export default async function MealDetails({
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import { getMeal } from '../../../../lib/meals';
+import styles from './page.module.css';
+
+export default async function MealDetailsPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
 
-  return <div>Meal Details for {slug}</div>;
+  const meal = await getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
