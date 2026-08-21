b7c188dcd391c892a4b1fb76e5f31e6f2758be2e
nehawork <neharamchandani.work@gmail.com>
2026-08-21 20:56:04 +0530

    Added Pages Metadata

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,8 +1,15 @@
 import MealsGrid from '@/components/MealsGrid';
 import MealsHeader from '@/components/MealsHeader';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
 import { getMeals } from '../../../lib/meals';
 import MealsLoading from './loading-out';
+
+export const metadata: Metadata = {
+  title: 'All Meals',
+  description:
+    'Browser the delicious meals shared by a food-loving vibrant community.',
+};
 
 async function Meals() {
   const meals = await getMeals();
